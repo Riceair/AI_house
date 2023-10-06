eddfcb601fddc3ff09b3d9eed58654ea8e3bd5ed
Riceair <handy00095@gmail.com>
2023-10-06 17:38:12 +0800

    multi layer mlp

--- model/mlp.py
+++ model/mlp.py
@@ -1,13 +1,47 @@
 import torch
 import torch.nn as nn
 
-class SingleMLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(SingleMLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)  # 第一層全連接層
-        self.fc2 = nn.Linear(hidden_dim, output_dim)  # 第二層全連接層
+class MultiMLP(nn.Module):
+    def __init__(self, input_dim : int, hidden_dims : list[int], output_dim : int, out_selection=None):
+        '''
+        input_dim: input dimension \n
+        hidden_dims: dimensions of hidden layers \n
+        output_dim: output dimension \n
+        out_selection: output activation funciont -> 'sigmoid', 'relu', 'tanh' \n
+        '''
+        super(MultiMLP, self).__init__()
+        layers = []
+        prev_dim = input_dim
 
+        # 動態隱藏層
+        for dim in hidden_dims:
+            layers.append(nn.Linear(prev_dim, dim))
+            layers.append(nn.ReLU())
+            prev_dim = dim
+        self.layers = nn.Sequential(*layers)
+
+        # 輸出層
+        self.output_layer = nn.Linear(prev_dim, output_dim)
+        # 輸出層激發函數
+        if out_selection == 'sigmoid':
+            self.output_activation = nn.Sigmoid()
+        elif out_selection == 'relu':
+            self.output_activation = nn.ReLU()
+        elif out_selection == 'tanh':
+            self.output_activation = nn.Tanh()
+        else:
+            self.output_activation = None
+    
     def forward(self, x):
-        out = torch.relu(self.fc1(x))  # 使用ReLU作為激活函數
-        out = self.fc2(out)
-        return out
+        x = self.layers(x)
+        x = self.output_layer(x)
+        if self.output_activation != None:
+            x = self.output_activation(x)
+        return x
+    
+if __name__ == "__main__":
+    input_dim = 40
+    hidden_dims = [128, 128,]
+    output_dim = 1
+    model = MultiMLP(input_dim, hidden_dims, output_dim, 'tanh')
+    print(model)
