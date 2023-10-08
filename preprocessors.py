6a5829d5f86695037ed2d2b7ba26739bba868452
Riceair <handy00095@gmail.com>
2023-10-08 19:57:17 +0800

    add external min distance

--- preprocessors.py
+++ preprocessors.py
@@ -10,6 +10,7 @@ class Preprocessor:
                  symbolic_names = symbolic_names,
                  numerical_names = numerical_names,
                  coordinate_names = coordinate_names,
+                 external_paths = external_paths,
                  truth_name = truth_name,
                  symbolic_type = "onehot",
                  num_min = 0, num_max = 1,
@@ -26,6 +27,7 @@ class Preprocessor:
         self.symbolic_names = symbolic_names
         self.numerical_names = numerical_names
         self.coordinate_names = coordinate_names
+        self.external_paths = external_paths
         self.num_min = num_min
         self.num_max = num_max
         self.y_min = y_min
@@ -56,16 +58,28 @@ class Preprocessor:
             normalizer = Normalizer(num_col, self.num_min, self.num_max)
             self.normalizer_num_dict[name] = normalizer # 紀錄 numerical normalizer
 
-        # coordinate columns transformer (twd97 -> lon, lat)
+        # coordinate columns transformer (twd97 -> lat, lng)
         self.coord_transformer = p_Transformer.from_crs("EPSG:3826", "EPSG:4326")  # TWD97 to WGS84
         self.normalizer_coord_dict = dict()
         h_col = df[coordinate_names[0]].to_numpy()
         v_col = df[coordinate_names[1]].to_numpy()
-        lons, lats = self.__convertTwd97toWgs84(h_col, v_col)
-        self.normalizer_coord_dict[coordinate_names[0]] = Normalizer(lons, self.num_min, self.num_max)
-        self.normalizer_coord_dict[coordinate_names[1]] = Normalizer(lats, self.num_min, self.num_max)
+        lats, lngs = self.__convertTwd97toWgs84(h_col, v_col)
 
-    def preprocess(self, csv_path):
+        # read external
+        ex_lats, ex_lngs = self.__getExLatLng(self.external_paths)
+        self.ex_coords_list = self.__getExCoordsList(self.external_paths) # 取得所有種類的 external 座標
+
+        # get min distance normalizer
+        min_ex_dists = self.__getMinExDist(lats, lngs, self.ex_coords_list)
+        self.normalizer_dist = Normalizer(min_ex_dists, self.num_min, self.num_max)
+
+        # get coordinate normalizer
+        lats = np.hstack([lats, ex_lats])
+        lngs = np.hstack([lngs, ex_lngs])
+        self.normalizer_coord_dict["lat"] = Normalizer(lats, self.num_min, self.num_max)
+        self.normalizer_coord_dict["lng"] = Normalizer(lngs, self.num_min, self.num_max)
+
+    def preprocess(self, csv_path, is_external=False):
         df = pd.read_csv(csv_path)
         df = pd.DataFrame(df)
 
@@ -89,39 +103,98 @@ class Preprocessor:
             normalizer = self.normalizer_num_dict[name]
             col = df[name].to_numpy()
             col = normalizer.normalize(col)
-            col = np.where(col<self.num_min, self.num_min, col) # 控制最大、最小值
-            col = np.where(col>self.num_max, self.num_max, col)
+            col = np.clip(col, self.num_min, self.num_max) # 控制最大、最小值
             x_nums.append(col)
         x_nums = np.stack(x_nums, axis=1)
 
         # normalize coordinate
-        x_coords = []
-        h_col = df[coordinate_names[0]].to_numpy()
-        v_col = df[coordinate_names[1]].to_numpy()
-        lons, lats = self.__convertTwd97toWgs84(h_col, v_col)
-        normalizer_lon = self.normalizer_coord_dict[coordinate_names[0]] # get normalizer
-        normalizer_lat = self.normalizer_coord_dict[coordinate_names[1]]
-        lons = normalizer_lon.normalize(lons)
-        lats = normalizer_lat.normalize(lats)
-        lons = np.where(lons<self.num_min, self.num_min, lons) # 控制最大、最小值
-        lons = np.where(lons>self.num_max, self.num_max, lons)
-        lats = np.where(lats<self.num_min, self.num_min, lats)
-        lats = np.where(lats>self.num_max, self.num_max, lats)
-        x_coords = np.stack([lons, lats], axis=1)
+        h_col = df[self.coordinate_names[0]].to_numpy()
+        v_col = df[self.coordinate_names[1]].to_numpy()
+        lats, lngs = self.__convertTwd97toWgs84(h_col, v_col)
+        normalizer_lat = self.normalizer_coord_dict["lat"] # get normalizer
+        normalizer_lng = self.normalizer_coord_dict["lng"]
+        normed_lats = normalizer_lat.normalize(lats)
+        normed_lngs = normalizer_lng.normalize(lngs)
+        normed_lats = np.clip(normed_lats, self.num_min, self.num_max) # 控制最大、最小值
+        normed_lngs = np.clip(normed_lngs, self.num_min, self.num_max)
+        x_coords = np.stack([normed_lats, normed_lngs], axis=1)
 
-        x = np.hstack([x_symbs, x_nums, x_coords])
+        if is_external:
+            ex_dists = self.__getMinExDist(lats, lngs, self.ex_coords_list)
+            ex_dists = self.normalizer_dist.normalize(ex_dists)
+            ex_dists = np.clip(ex_dists, self.num_min, self.num_max)
+            x = np.hstack([x_symbs, x_nums, x_coords, ex_dists])
+        else:
+            x = np.hstack([x_symbs, x_nums, x_coords])
+
         return x
 
     def denormalizeY(self, y_pred):
         return self.normalizer_y.denormalize(y_pred)
 
+    def __getMinExDist(self, lats, lngs, ex_coords_list):
+        def getMinDist(coord, ex_coords): # 取得距離最短數據
+            coord_tile = np.tile(coord, (len(ex_coords), 1)) # 複製 n 次
+            # 歐式距離計算 (用numpy矩陣計算加快速度)
+            coord_div = coord_tile - ex_coords
+            coord_squ = np.square(coord_div)
+            coord_add = coord_squ[:,0] + coord_squ[:,1]
+            dists = np.sqrt(coord_add)
+            return dists.min()
+
+        min_ex_dists = []
+        for lat, lng in zip(lats, lngs):
+            coord = np.array((lat, lng))
+            min_dists = [getMinDist(coord, ex_coords) for ex_coords in ex_coords_list]
+            min_ex_dists.append(np.array(min_dists))
+        min_ex_dists = np.array(min_ex_dists)
+        return min_ex_dists
+
     def __convertTwd97toWgs84(self, h_col, v_col):
-        lons, lats = [], []
+        lats, lngs = [], []
         for h, v in zip(h_col, v_col):
-            lon, lat = self.coord_transformer.transform(h, v)
-            lons.append(lon)
+            lat, lng = self.coord_transformer.transform(h, v)
             lats.append(lat)
-        return np.array(lons), np.array(lats)
+            lngs.append(lng)
+        return np.array(lats), np.array(lngs)
+
+    def __getExLatLng(self, external_paths):
+        ex_lats, ex_lngs = [], []
+        for path in external_paths:
+            df = pd.read_csv(path)
+            df = pd.DataFrame(df)
+            lats = df["lat"].to_list()
+            lngs = df["lng"].to_list()
+
+            # 去除不合規定的座標
+            for lat, lng, in zip(lats, lngs):
+                if pd.isna(lat) or pd.isna(lng):
+                    continue
+                if lat < 0 or lng < 0:
+                    continue
+                ex_lats.append(lat)
+                ex_lngs.append(lng)
+
+        return np.array(ex_lats), np.array(ex_lngs)
+    
+    def __getExCoordsList(self, external_paths):
+        ex_coords_list = []
+        for path in external_paths:
+            df = pd.read_csv(path)
+            df = pd.DataFrame(df)
+            lats = df["lat"].to_list()
+            lngs = df["lng"].to_list()
+            coords = []
+            # 去除不合規定的座標
+            for lat, lng, in zip(lats, lngs):
+                if pd.isna(lat) or pd.isna(lng):
+                    continue
+                if lat < 0 or lng < 0:
+                    continue
+                coords.append([lat, lng])
+            ex_coords_list.append(np.array(coords))
+
+        return ex_coords_list
 
     def __setSymoblicType(self, symoblic_type):
         if symoblic_type == "onehot":
@@ -135,7 +208,7 @@ class Preprocessor:
 
 if __name__ == "__main__":
     preprocessor = Preprocessor(train_path, symbolic_type='onehot')
-    x = preprocessor.preprocess(public_path)
+    x = preprocessor.preprocess(public_path, is_external=True)
     print(x.shape)
 
     y = preprocessor.y_true
--- regression_rf.py
+++ regression_rf.py
@@ -6,11 +6,12 @@ import numpy as np
 import joblib
 
 # preprocessing
-symbolic_type = "onehot"
-save_path = "save_models/rf_"+symbolic_type+".pkl"
-result_path = "result/rf_"+symbolic_type+".csv"
-preprocessor = Preprocessor(symbolic_type=symbolic_type, num_min=0, num_max=1)
-X_train = preprocessor.preprocess(train_path)
+symbolic_type = "order"
+save_path = "save_models/rf_external.pkl"
+result_path = "result/rf_external.csv"
+is_external = True
+preprocessor = Preprocessor(symbolic_type=symbolic_type, num_min=0, num_max=1, y_min=0, y_max=1)
+X_train = preprocessor.preprocess(train_path, is_external=is_external)
 y_train = preprocessor.y_true
 y_train = np.ravel(y_train)
 
@@ -19,7 +20,7 @@ y_train = np.ravel(y_train)
 # joblib.dump(model, save_path)
 model = joblib.load(save_path)
 
-X_test = preprocessor.preprocess(public_path)
+X_test = preprocessor.preprocess(public_path, is_external=is_external)
 y_test = model.predict(X_test)
 y_test = preprocessor.denormalizeY(y_test)
 
